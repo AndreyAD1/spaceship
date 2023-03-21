dbca19d0f55e2bf3f50308774554297a7fe40e8e
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-03-21 18:49:20 +0200

    add a meteorite draft

--- spaceship.go
+++ spaceship.go
@@ -5,11 +5,25 @@ import (
 	"time"
 
 	"github.com/AndreyAD1/spaceship/frame"
+	"github.com/gdamore/tcell/v2"
 )
+
+func drawMeteorite(screen tcell.Screen) {
+	width, height := screen.Size()
+	meteoriteStyle := tcell.StyleDefault.Background(tcell.ColorReset)
+	meteorColumn := width / 2
+	for line := 0; line < height - 3; line++ {
+		screen.SetContent(meteorColumn, line, 'O', nil, meteoriteStyle)
+		screen.Show()
+		time.Sleep(250 * time.Millisecond)
+		screen.Clear()
+	}
+	screen.Show()
+}
 
 func main() {
 	fmt.Println("a spaceship game will be here")
 	newFrame := frame.DrawFrame()
 	defer newFrame.Clear()
-	time.Sleep(5 * time.Second)
+	drawMeteorite(newFrame)
 }
