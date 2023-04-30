a5404be53d23d2233dc5d91237418ed5eb8885ef
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-04-30 19:49:09 +0300

    add another test

--- internal/services/meteorites_test.go
+++ internal/services/meteorites_test.go
@@ -38,19 +38,71 @@ func TestMeteorite_MoveAndDeactivate(t *testing.T) {
 	case <-time.After(1 * time.Second):
 		t.Errorf("no meteorites in the object channel")
 	}
-	require.True(t, meteorite.IsBlocked)
-	MockedScreenSvc.AssertCalled(t, "GetScreenSize")
-
-	select {
-	case receivedMeteorite := <-objectChannel:
-		t.Errorf("a blocked meteorite appears in the channel")
-		require.Equal(t, meteorite, receivedMeteorite)
-	case <-time.After(100 * time.Millisecond):
-	}
-
+	MockedScreenSvc.AssertNumberOfCalls(t, "GetScreenSize", 1)
 	require.True(t, meteorite.IsBlocked)
 	require.True(t, meteorite.Active)
 	expectedY := initialY + meteorite.Speed
 	require.Equal(t, expectedY, meteorite.Y)
-	MockedScreenSvc.AssertNumberOfCalls(t, "GetScreenSize", 1)
+
+	select {
+	case <-objectChannel:
+		t.Errorf("a blocked meteorite appears in the channel")
+	case <-time.After(100 * time.Millisecond):
+	}
+
+	meteorite.IsBlocked = false
+
+	select {
+	case receivedMeteorite := <-objectChannel:
+		require.Equal(t, meteorite, receivedMeteorite)
+	case <-time.After(1 * time.Second):
+		t.Errorf("no meteorites in the object channel")
+	}
+	MockedScreenSvc.AssertNumberOfCalls(t, "GetScreenSize", 2)
+	require.True(t, meteorite.IsBlocked)
+	require.True(t, meteorite.Active)
+	expectedY += meteorite.Speed
+	require.Equal(t, expectedY, meteorite.Y)
+
+	meteorite.Active = false
+	select {
+	case <-objectChannel:
+		t.Errorf("a deactivated meteorite appears in the channel")
+	case <-time.After(100 * time.Millisecond):
+	}
+}
+
+func TestMeteorite_MoveAndLeaveScreen(t *testing.T) {
+	objectChannel := make(chan services.ScreenObject)
+	initialY, speed := float64(2), float64(1)
+	baseObject := services.BaseObject{
+		false,
+		false,
+		true,
+		0,
+		initialY,
+		tcell.StyleDefault.Background(tcell.ColorReset),
+		speed,
+		services.MeteoriteView,
+	}
+	MockedScreenSvc := mocks.NewScreenSvc(t)
+	MockedScreenSvc.On("GetScreenSize").Return(1, 1)
+	meteorite := &services.Meteorite{
+		BaseObject: baseObject,
+		Objects:    chan<- services.ScreenObject(objectChannel),
+		ScreenSvc:  MockedScreenSvc,
+	}
+	go meteorite.Move()
+	select {
+	case <- objectChannel:
+	case <- time.After(100 * time.Millisecond):
+	}
+	meteorite.IsBlocked = false
+	select {
+	case receivedMeteorite := <-objectChannel:
+		t.Errorf("a meteorite left the screen but appeared in the channel")
+		require.Equal(t, meteorite, receivedMeteorite)
+	case <-time.After(100 * time.Millisecond):
+	}
+	require.False(t, meteorite.Active)
 }
