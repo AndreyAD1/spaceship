cb8dbbca375e57995ff49d577db37e14133b3ce1
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-05-19 22:58:02 +0300

    add star blink

--- internal/services/stars.go
+++ internal/services/stars.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"math/rand"
+	"time"
 
 	"github.com/gdamore/tcell/v2"
 )
@@ -21,29 +22,69 @@ func GenerateStars(starsChan chan ScreenObject, screenSvc *ScreenService) {
 				break
 			}
 		}
+		style := tcell.StyleDefault.Background(tcell.ColorReset).Normal()
 		baseObject := BaseObject{
 			false,
 			false,
 			true,
 			float64(starColumn),
 			float64(starLine),
-			tcell.StyleDefault.Background(tcell.ColorReset),
+			style,
 			0,
 			"*",
 		}
-		star := Star{baseObject, starsChan}
+		head := styleFuncChain{
+			style.Bold,
+			true,
+			nil,
+		}
+		next := styleFuncChain{
+			style.Bold,
+			false,
+			nil,
+		}
+		next2 := styleFuncChain{
+			style.Dim,
+			true,
+			nil,
+		}
+		next3 := styleFuncChain{
+			style.Dim,
+			false,
+			nil,
+		}
+		next3.next = &head
+		next2.next = &next3
+		next.next = &next2
+		head.next = &next
+		star := Star{baseObject, starsChan, &head}
 		go star.Blink()
 		usedCoords[starLine][starColumn] = true
 	}
 }
 
+type styleFuncChain struct {
+	self func(bool) tcell.Style
+	argument bool
+	next *styleFuncChain
+}
+
 type Star struct {
 	BaseObject
 	StarChan   chan<- ScreenObject
+	styleChain *styleFuncChain
 }
 
 func (star *Star) Blink() {
+	tickOffset := time.Duration(rand.Intn(400) + 150)
+	ticker := time.NewTicker(tickOffset * time.Millisecond)
 	for {
 		star.StarChan <- star
+		select {
+		case <-ticker.C:
+			star.styleChain = star.styleChain.next
+			star.Style = star.styleChain.self(star.styleChain.argument)
+		default:
+		}
 	}
 }
