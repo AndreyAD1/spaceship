71e72e08cb92472e39e878758c4d0c7db0529b3e
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-06-18 20:58:58 +0300

    shorten the code

--- internal/services/spaceship.go
+++ internal/services/spaceship.go
@@ -40,35 +40,35 @@ type Spaceship struct {
 	Objects   chan<- ScreenObject
 	ScreenSvc *ScreenService
 	gameover  chan *BaseObject
-	Vx float64
-	Vy float64
+	Vx        float64
+	Vy        float64
+}
+
+func (spaceship *Spaceship) getNewSpeed(
+	velocity,
+	acceleration,
+	frictionCoef float64,
+) float64 {
+	speedRate := velocity / spaceship.MaxSpeed
+	accelerationRate := math.Cos(speedRate) * 0.75
+	newSpeed := velocity + acceleration*accelerationRate
+	return newSpeed * frictionCoef
 }
 
 func (spaceship *Spaceship) apply_acceleration(ax, ay float64) {
-	xSpeedRate := spaceship.Vx / spaceship.MaxSpeed
-	ySpeedRate := spaceship.Vx / spaceship.MaxSpeed
-
-	axRate := math.Cos(xSpeedRate) * 0.75
-	ayRate := math.Cos(ySpeedRate) * 0.75
-
-	spaceship.Vx += ax * axRate
-	spaceship.Vy += ay * ayRate
-
-	spaceship.Vx *= 0.9
-	spaceship.Vy *= 0.95
-
-	// change coordinates
+	spaceship.Vx = spaceship.getNewSpeed(spaceship.Vx, ax, 0.9)
+	spaceship.Vy = spaceship.getNewSpeed(spaceship.Vy, ay, 0.95)
 	newX := spaceship.X + spaceship.Vx
 	newY := spaceship.Y + spaceship.Vy
-	leftBoundaryIsValid := spaceship.ScreenSvc.IsInsideScreen(newX, spaceship.Y)
-	rightBoundaryIsValid := spaceship.ScreenSvc.IsInsideScreen(newX+3, spaceship.Y)
-	if !leftBoundaryIsValid || !rightBoundaryIsValid {
+	leftBoundaryIsOk := spaceship.ScreenSvc.IsInsideScreen(newX, spaceship.Y)
+	rightBoundaryIsOk := spaceship.ScreenSvc.IsInsideScreen(newX+3, spaceship.Y)
+	if !leftBoundaryIsOk || !rightBoundaryIsOk {
 		spaceship.Vx = 0
 		newX = spaceship.X
 	}
-	upperBoundaryIsValid := spaceship.ScreenSvc.IsInsideScreen(spaceship.X, newY)
-	lowerBoundaryIsValid := spaceship.ScreenSvc.IsInsideScreen(spaceship.X, newY+5)
-	if !upperBoundaryIsValid || !lowerBoundaryIsValid {
+	upperBoundaryIsOk := spaceship.ScreenSvc.IsInsideScreen(spaceship.X, newY)
+	lowerBoundaryIsOk := spaceship.ScreenSvc.IsInsideScreen(spaceship.X, newY+5)
+	if !upperBoundaryIsOk || !lowerBoundaryIsOk {
 		spaceship.Vy = 0
 		newY = spaceship.Y
 	}
@@ -88,7 +88,12 @@ func (spaceship *Spaceship) Move() {
 		case GoDown:
 			spaceship.apply_acceleration(0, 0.1)
 		case Shoot:
-			go Shot(spaceship.ScreenSvc, spaceship.Objects, spaceship.X+2, spaceship.Y-1)
+			go Shot(
+				spaceship.ScreenSvc,
+				spaceship.Objects,
+				spaceship.X+2,
+				spaceship.Y-1,
+			)
 		case NoEvent:
 			spaceship.apply_acceleration(0, 0)
 		}
