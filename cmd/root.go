a809152ba1b2450bb58bee9d2ede9e7447677075
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2023-08-20 18:23:22 +0300

    add all flags to the new cli interface

--- cmd/root.go
+++ cmd/root.go
@@ -1,42 +1,48 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
 
+var debug *bool
+var logFile *string
+var cpuProfile *string
+
 var rootCmd = &cobra.Command{
-	Use:   "",
+	Use:   "spaceship.exe",
 	Short: "A small console game",
 	Long: `This is an old-fashioned console game. 
 A user controls a spaceship flying through a meteor shower. 
 A user's goal is to destroy meteorites and not collide with them.`,
-	Run: func(cmd *cobra.Command, args []string) { run() },
+	Run: run,
 }
 
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.spaceship.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	debug = rootCmd.Flags().BoolP("debug", "d", false, "Run in a debug mode")
+	logFile = rootCmd.Flags().StringP(
+		"log_file", 
+		"l", 
+		"", 
+		"write logs to this file",
+	)
+	cpuProfile = rootCmd.Flags().StringP(
+		"cpuprofile", 
+		"c", 
+		"", 
+		"write a cpu profile to this file",
+	)
 }
 
 
--- cmd/run.go
+++ cmd/run.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -13,26 +12,23 @@ import (
 	"github.com/AndreyAD1/spaceship/internal/config"
 	"github.com/AndreyAD1/spaceship/internal/logger"
 	"github.com/caarlos0/env/v7"
+	"github.com/spf13/cobra"
 )
 
-func run() {
-	debug := flag.String("debug", "", "run in a debug mode")
-	logFile := flag.String("log_file", "", "write logs to this file")
-	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to file")
-	flag.Parse()
+func run(cmd *cobra.Command, args []string) {
 	configuration := config.StartupConfig{}
 	err := env.Parse(&configuration)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if *debug != "" {
-		configuration.Debug = *debug == "true"
+	if *debug {
+		configuration.Debug = true
 	}
 	if *logFile != "" {
 		configuration.LogFile = *logFile
 	}
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
+	if *cpuProfile != "" {
+		f, err := os.Create(*cpuProfile)
 		if err != nil {
 			log.Fatal(err)
 		}
